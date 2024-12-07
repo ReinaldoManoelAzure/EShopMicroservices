052a9a90cd77085cf3f3f6761f1af2c3c87ba3c6
Reinaldo Manoel Figueiredo De Freitas <reinaldomanoelazure@gmail.com>
2024-12-07 16:55:06 -0300

    Início do desenvolvimento do Serviço de Catalogo de Produtos

---
+++ src/BuildingBlocks/BuildingBlocks/Endpoints/CarterModuleLoader.cs
@@ -0,0 +1,16 @@
+﻿using Carter;
+using System.Reflection;
+
+namespace BuildingBlocks.Endpoints
+{
+    public class DependencyContextAssemblyCatalogCustom(Type programType) : DependencyContextAssemblyCatalog
+    {
+
+        public override IReadOnlyCollection<Assembly> GetAssemblies()
+        {
+            return new List<Assembly> { programType.Assembly };
+        }
+
+        
+    }
+}
